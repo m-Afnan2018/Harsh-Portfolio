95d6441ce4f5094cada29d7f97d14033c3471aac
m-Afnan2018 <m.afnan2018@gmail.com>
2026-01-28 13:53:13 +0000

    Updated

--- components/home-personal/Header.jsx
+++ components/home-personal/Header.jsx
@@ -21,7 +21,7 @@ function Header() {
   return (
     <div
       className="header header-personal valign bg-img"
-      data-background="/assets/imgs/header/p0.jpg"
+      data-background="/assets/imgs/header/p0.png"
       data-overlay-dark="2"
     >
       <div className="container ontop">
